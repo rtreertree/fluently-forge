60457be5c22635bfd164ce0f50ac8c8b9b606a69
rtreertree <>
2025-04-14 19:34:32 +0100

    Fixed settings update Everythings are working

--- actions/change-settings.tsx
+++ actions/change-settings.tsx
@@ -6,22 +6,23 @@ import { getUserById } from '@/data/user';
 import { db } from '@/lib/db';
 
 export const changeSettings = async (userId: string, values: z.infer<typeof SettingsSchema>) => {
-const validatedField = SettingsSchema.safeParse(values);
+    console.log("Change settings", { userId, values });
+    const validatedField = SettingsSchema.safeParse(values);
     if (!validatedField.success) {
         return { error: "Invalid field" };
     }
 
-    const {name, english_level } = validatedField.data;
+    const { name, english_level } = validatedField.data;
     try {
-         await db.user.update({
-                    where: {
-                        id: userId,
-                    },
-                    data: {
-                        name: name,
-                        englishLevel: english_level,
-                    },
-                });
+        await db.user.update({
+            where: {
+                id: userId,
+            },
+            data: {
+                name: name,
+                englishLevel: english_level,
+            },
+        });
 
         return { success: `Settings changed` };
 
