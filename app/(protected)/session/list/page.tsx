d5fc3618b37f3c4fedfcf3df87694b3b6481426b
rtreertree <rtreertree@gmail.com>
2025-07-12 21:34:34 +0700

    commit test

--- app/(protected)/session/list/page.tsx
+++ app/(protected)/session/list/page.tsx
@@ -2,11 +2,29 @@
 
 // import { SessionMonologue } from "../../_components/session/monologue/session-monologue";
 import Loader from "@/components/suspend/loading";
+import { ListBox } from "../../_components/session/list/session-list";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import router from "next/router";
 
 
 const SessionListPage = () => {
+    const session = useSession();
+    const [isLoading, setIsLoading] = useState(true);
+
+    // Check if session is loading or not
+    if (!session.data?.user) {
+        router.push("/auth/login");
+    }
+
+
+    // Simulate loading data
+    setTimeout(() => {
+        setIsLoading(false);
+    }, 1000);
+
     return (
-        <Loader text={"NOT IMPLEMENT ...."}/>
+        isLoading ? <Loader text={""} /> : <ListBox data={[]} />
     );
 };
 
